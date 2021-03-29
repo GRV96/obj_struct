45836c125be24269863c5653c2a1fca8c2a36b07
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-03-28 22:11:20 -0400

    Containers' class written in object structure

--- obj_struct.py
+++ obj_struct.py
@@ -19,6 +19,7 @@ def _obj_is_a_dlst(obj):
 def write_obj_struct(struct, output_path):
 	if isinstance(output_path, str):
 		output_path = Path(output_path)
+
 	elif isinstance(output_path, Path):
 		raise TypeError("The given path must be an instance "
 			+ "of str or Pathlib's class Path.")
@@ -35,12 +36,15 @@ def _write_obj_struct_rec(obj_to_write, w_stream, indent=0):
 
 		for i in range(length):
 			item = obj_to_write[i]
+			line = tabs + "[" + str(i) + "]: "
 
 			if _obj_is_a_dlst(item):
+				line += str(type(item))
+				w_stream.write(line + "\n")
 				_write_obj_struct_rec(item, w_stream, indent)
+
 			else:
-				line = tabs + "[" + str(i) + "]: "\
-					+ _obj_and_type_to_str(item)
+				line += _obj_and_type_to_str(item)
 				w_stream.write(line + "\n")
 
 	elif isinstance(obj_to_write, dict):
@@ -48,18 +52,25 @@ def _write_obj_struct_rec(obj_to_write, w_stream, indent=0):
 			line = tabs + str(key) + ":"
 
 			if _obj_is_a_dlst(value):
+				line += str(type(value))
+				w_stream.write(line + "\n")
 				_write_obj_struct_rec(value, w_stream, indent)
+
 			else:
 				line += " " + _obj_and_type_to_str(value)
-
-			w_stream.write(line + "\n")
+				w_stream.write(line + "\n")
 
 	elif isinstance(obj_to_write, set):
 		for item in obj_to_write:
+			line = tabs
+
 			if _obj_is_a_dlst(item):
+				line += str(type(item))
+				w_stream.write(line + "\n")
 				_write_obj_struct_rec(item, w_stream, indent)
+
 			else:
-				line = tabs + _obj_and_type_to_str(item)
+				line += _obj_and_type_to_str(item)
 				w_stream.write(line + "\n")
 
 	else:
