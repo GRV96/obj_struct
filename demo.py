0526aed9b205dc46296464b4705923f28a940f8a
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-03-28 21:44:51 -0400

    An object structure is written.

---
+++ demo.py
@@ -0,0 +1,50 @@
+from obj_struct import write_obj_struct
+
+
+def repr_arg(an_arg):
+	if isinstance(an_arg, str):
+		return "'" + an_arg + "'"
+
+	return str(an_arg)
+
+
+class Ajxo: # "thing" in Esperanto
+	def __init__(self, a, b):
+		self.a = a
+		self.b = b
+
+	def __repr__(self):
+		return self.__class__.__name__ + "(" + repr_arg(self.a) + ", "\
+			+ repr_arg(self.b) + ")"
+
+
+#structure = "struct"
+structure = ( # tuple
+	Ajxo(None, 2187),
+	11,
+	{ # set
+		"abc",
+		7.42,
+		( # tuple
+			"def",
+			66,
+			Ajxo(77, 5.6)),
+		"xyz"},
+	{ # dict
+		(Ajxo("g", "h"), "u"): "something",
+		"ghi": Ajxo(8, 9),
+		},
+	Ajxo([3, "p", 2.09], Ajxo("q", 1)),
+	[ # list
+		63485,
+		{ # dict
+			4: Ajxo(2, 2),
+			Ajxo(19, 23): [ # list
+				"i",
+				"j",
+				"k",
+				"l"],
+			"fr": "quelque chose"},
+		3.4159])
+
+write_obj_struct(structure, "result.txt")
---
+++ obj_struct.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+
+
+_DLST = (dict, list, set, tuple)
+
+
+def _make_tabs(n):
+	return "\t" * n
+
+
+def _obj_and_type_to_str(obj):
+	return str(obj) + " " + str(type(obj))
+
+
+def _obj_is_a_dlst(obj):
+	return isinstance(obj, _DLST)
+
+
+def write_obj_struct(struct, output_path):
+	if isinstance(output_path, str):
+		output_path = Path(output_path)
+	elif isinstance(output_path, Path):
+		raise TypeError("The given path must be an instance "
+			+ "of str or Pathlib's class Path.")
+
+	_write_obj_struct_rec(struct, output_path.open(mode="w"))
+
+
+def _write_obj_struct_rec(obj_to_write, w_stream, indent=0):
+	tabs = _make_tabs(indent)
+	indent += 1
+
+	if isinstance(obj_to_write, (list, tuple)):
+		length = len(obj_to_write)
+
+		for i in range(length):
+			item = obj_to_write[i]
+
+			if _obj_is_a_dlst(item):
+				_write_obj_struct_rec(item, w_stream, indent)
+			else:
+				line = tabs + "[" + str(i) + "]: "\
+					+ _obj_and_type_to_str(item)
+				w_stream.write(line + "\n")
+
+	elif isinstance(obj_to_write, dict):
+		for key, value in obj_to_write.items():
+			line = tabs + str(key) + ":"
+
+			if _obj_is_a_dlst(value):
+				_write_obj_struct_rec(value, w_stream, indent)
+			else:
+				line += " " + _obj_and_type_to_str(value)
+
+			w_stream.write(line + "\n")
+
+	elif isinstance(obj_to_write, set):
+		for item in obj_to_write:
+			if _obj_is_a_dlst(item):
+				_write_obj_struct_rec(item, w_stream, indent)
+			else:
+				line = tabs + _obj_and_type_to_str(item)
+				w_stream.write(line + "\n")
+
+	else:
+		line = tabs + _obj_and_type_to_str(obj_to_write)
+		w_stream.write(line + "\n")
